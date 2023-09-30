0919dae17ce261463c408ad73bc805a3427d4461
dayton-braison <daytonbraison@gmail.com>
2023-09-30 11:49:55 -0500

    Another commit

--- amor_app/views.py
+++ amor_app/views.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'landing_page.html')
 
+=======
+
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'landing_page.html')
+>>>>>>> 752a8f7 (Another commit)
